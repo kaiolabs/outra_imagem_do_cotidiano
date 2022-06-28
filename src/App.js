93a5b41c4aab8c4c3594a01586c7a960ca3ad775
kaiolabs <peypro1@gmail.com>
2022-06-27 22:16:24 -0300

    update

--- src/App.js
+++ src/App.js
@@ -64,35 +64,36 @@ function App() {
             </div>
             <img src={img6} className='imgParte1'/>
           </div>
-          <h3>O país que virou 'lixão' de roupas usadas dos países ricos</h3>
+          
           <div className='documentario'>
+          <h3 className='h3Video'>O país que virou 'lixão' de roupas usadas dos países ricos</h3>
           <iframe  src="https://www.youtube.com/embed/enektksvTqI?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
         </div>
         <div className='boxInfo'>
           <h2>Dados do grupo</h2>
           <p><span>Titulo:</span> Outra Imagem do Cotidiano.</p>
+          <hr className='hr'/>
           <p><span>Subtítulo:</span> Fast fashion: a moda rápida e descartável</p>
+          <hr className='hr'/>
           <p><span>Descrição:</span> O conceito de fast fashion (ou “moda rápida”, em português) surgiria apenas na década de 1990, com o barateamento tanto da mão de obra quanto da matéria-prima na indústria têxtil. Lojas como Zara, H&M e Topshop investiram em peças que lembravam a alta costura, porém tinham custo baixo para o consumidor e um tempo de vida reduzido.</p>
-          <p><span>Materiais Utilizados:</span>Para desenvolvermos o site utilizamos ReactJS.</p>
+          <hr className='hr'/>
+          <p><span>Materiais Utilizados:</span> Para desenvolvermos esse site foi utilizado Reactjs, que é uma biblioteca de JavaScript, utilizada para desenvolver web sites.</p>
+          <hr className='hr'/>
+          <p><span>Desenvolvido por:</span> Grupo 5</p>
+          <p>Kaio Vinicius de Paiva Rodrigues, Isabelle Pinto Vargas, Mylena Eduarda Vogt Stuelp, Murilo Tatsch Pereira</p>
+          <hr className='hr'/>
           <p><span>Referências:</span></p>
-          <p>Fonte: <a href="https://veja.abril.com.br/economia/fast-fashion-a-moda-rapida-e-descartavel-agora-em-versao-on-line/">https://veja.abril.com.br/economia/fast-fashion-a-moda-rapida-e-descartavel-agora-em-versao-on-line/</a></p>
-            <p>Fonte: <a href="https://sammy-carvalho.medium.com/a-ind%C3%BAstria-do-fast-fashion-e-por-que-isso-tamb%C3%A9m-%C3%A9-problema-seu-6e686fcdaa80#:~:text=Lojas%20como%20C%26A%2C%20Renner%2C%20Marisa,que%20se%20consome%20fast%2Dfashion">"https://sammy-carvalho.medium.com/</a></p>
-            <p>Fonte: <a href="https://reporterbrasil.org.br/2016/08/a-moda-de-explorar-o-trabalhador/">https://reporterbrasil.org.br/2016/08/a-moda-de-explorar-o-trabalhador/</a></p>
-            <p>Fonte: <a href="https://istoe.com.br/332682_OS+GIGANTES+DA+MODA+RAPIDA/">https://istoe.com.br/332682_OS+GIGANTES+DA+MODA+RAPIDA/</a></p>
-            <p>Fonte: <a href="https://revistagalileu.globo.com/Revista/noticia/2016/06/escravos-da-moda-os-bastidores-nada-bonitos-da-industria-fashion.html">https://revistagalileu.globo.com/Revista/noticia/2016/06/escravos-da-moda-os-bastidores-nada-bonitos-da-industria-fashion.html</a></p>
-            <p>Fonte: <a href="https://www.folhaum.com/post/fast-fashion-e-a-escravid%C3%A3o-contempor%C3%A2nea/">https://www.folhaum.com/post/fast-fashion-e-a-escravid%C3%A3o-contempor%C3%A2nea</a></p>
-            <p>Fonte: <a href="https://www.youtube.com/watch?v=U-FZnFcKEHA&ab_channel=DWBrasil/">https://www.youtube.com/watch?v=U-FZnFcKEHA&ab_channel=DWBrasil</a></p>
-            <p>Fonte: <a href="https://www.youtube.com/watch?v=enektksvTqI&ab_channel=BBCNewsBrasil/">https://www.youtube.com/watch?v=enektksvTqI&ab_channel=BBCNewsBrasil</a></p>            
+          <p>BRITO, Sabrina; Fast fashion: a moda rápida e descartável, agora em versão on-line; Veja. Disponível em: <a href="https://veja.abril.com.br/economia/fast-fashion-a-moda-rapida-e-descartavel-agora-em-versao-on-line/">https://veja.abril.com.br</a> Acesso em 26 de junho de 2022</p>
+            <p>CARVALHO, Samanta; A indústria do fast-fashion e por que isso também é problema seu. Disponível em: <a href="https://sammy-carvalho.medium.com/a-ind%C3%BAstria-do-fast-fashion-e-por-que-isso-tamb%C3%A9m-%C3%A9-problema-seu-6e686fcdaa80#:~:text=Lojas%20como%20C%26A%2C%20Renner%2C%20Marisa,que%20se%20consome%20fast%2Dfashion">"https://sammy-carvalho.medium.com</a> Acesso em 26 de junho de 2022</p>
+            <p>GOMES, MARCEL; A moda de explorar o trabalhador; Reporter Brasil. Disponível em: <a href="https://reporterbrasil.org.br/2016/08/a-moda-de-explorar-o-trabalhador/">https://reporterbrasil.org.br</a> Acesso em 26 de junho de 2022</p>
+            <p>MESTRE, NATÁLIA; Os gigantes da moda rápida; ISTOÉ. Disponível em: <a href="https://istoe.com.br/332682_OS+GIGANTES+DA+MODA+RAPIDA/">https://istoe.com.br</a> Acesso em 26 de junho de 2022</p>
+            <p>TANJI, THIAGO; Escravos da moda: os bastidores nada bonitos da indústria fashion; Revista Galileu. Disponível em: <a href="https://revistagalileu.globo.com/Revista/noticia/2016/06/escravos-da-moda-os-bastidores-nada-bonitos-da-industria-fashion.html">https://revistagalileu.globo.com</a> Acesso em 26 de junho de 2022</p>
+            <p>DW Brasil; "Fast fashion": moda barata, rápida e sustentável; Disponível em: <a href="https://www.youtube.com/watch?v=U-FZnFcKEHA&ab_channel=DWBrasil/">https://www.youtube.com</a> Acesso em 26 de junho de 2022</p>
+            <p>BBC News Brasil; O país que virou 'lixão' de roupas usadas dos países ricos; Disponível em:<a href="https://www.youtube.com/watch?v=enektksvTqI&ab_channel=BBCNewsBrasil/">https://www.youtube.com</a> Acesso em 26 de junho de 2022</p>        
+            <hr className='hr'/>
         </div>
         </div>
-        <footer>
-          <div>
-            <p>Desenvolvido por: Grupo 5</p>
-            <p>Integrantes: Kaio Vinicius de Paiva Rodrigues, Isabelle Pinto Vargas, Mylena Eduarda Vogt Stuelp, Murilo Tatsch Pereira</p>
-          </div>
-          <img src={img7}/>
-        </footer>
     </div>
   );
 }
